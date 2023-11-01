0517a8af7d1680d3e627abf6b8269fb93eda4599
davileahy <davileahy2@gmail.com>
2023-11-01 20:31:44 -0300

    first commit
    co-authored by:
    Moriarthyy
    <samuellove123456@gmail.com>
    Oslecjr
    <oslecjr@gmail.com>

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDp-uWECwNU_UJ_1OiUBxQJEihy6zV_VTA",
+    authDomain: "menudigital3d-201b7.firebaseapp.com",
+    projectId: "menudigital3d-201b7",
+    storageBucket: "menudigital3d-201b7.appspot.com",
+    messagingSenderId: "393728349892",
+    appId: "1:393728349892:web:3609a64eab73b2ac36db91",
+    measurementId: "G-0DWXZ06YWQ"
+};
+firebase.initializeApp(firebaseConfig)
---
+++ login/script.js
@@ -0,0 +1,31 @@
+const form = {
+    email: () => document.getElementById('email'),
+    password: () => document.getElementById('password'),
+    confirmPassword: () => document.getElementById('confirmPassword')
+}
+
+
+
+function createAccount() {
+    const password = form.password().value
+    const confirmPassword = form.confirmPassword().value
+    if (confirmPassword != password) {
+        // Exibir mensagem de "Senhas diferents!"
+        window.alert('Algo deu errado, tente novamente.')
+    }
+    else if (confirmPassword === password) {
+        // Realizar cadastro.
+        cadastro()
+    }    
+
+}
+function cadastro() {
+    const email = form.email().value
+    const password = form.password().value
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+        window.alert("Login efetuado com sucesso!")
+    }).catch(error => {
+        window.alert(error)
+    })
+}
+
